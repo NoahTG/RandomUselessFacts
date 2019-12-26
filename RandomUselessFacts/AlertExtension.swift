9c8929d7f2edcee37c5c1257f3a41f6aabd0e07b
NoahTG <26232817+NoahTG@users.noreply.github.com>
2019-12-26 12:57:06 -0800

    add delete func to alert extension

--- RandomUselessFacts/AlertExtension.swift
+++ RandomUselessFacts/AlertExtension.swift
@@ -17,7 +17,7 @@ extension UIViewController {
     ///     - title:  the message to be displayed to the user.
     ///     - message:  Detailed body message.
     /// - Returns: the configured error alert controller.
-   
+    
     func showAlert(title: String, message: String?){
         let alertVC = UIAlertController(title: title,
                                         message: message,
@@ -25,6 +25,26 @@ extension UIViewController {
         alertVC.addAction(UIAlertAction(title: "OK",
                                         style: .default,
                                         handler: nil))
+        DispatchQueue.main.async {
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func showDeleteFactAlert() {
+        let alertVC = UIAlertController(title: "Delete Fact",
+                                        message: "Do you want to delete this fact?",
+                                        preferredStyle: .alert)
+        
+        alertVC.addAction(UIAlertAction(title: "Cancel",
+                                        style: .cancel,
+                                        handler: nil))
+        
+        alertVC.addAction(UIAlertAction(title: "Delete",
+                                        style: .destructive,
+                                        handler: {
+            (alert: UIAlertAction!) -> Void in
+        }))
         DispatchQueue.main.async {
             self.present(alertVC, animated: true, completion: nil)
         }
